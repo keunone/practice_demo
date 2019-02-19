300e2201b60701546c62b9c48b14ca6f45f3c675
Zoey <746542397@qq.com>
2019-02-19 17:17:42 +0800

    feat：word插件实践项目

---
+++ office-add-in/src/components/App.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Header } from './Header';
+import { Content } from './Content';
+// import Progress from './Progress';
+
+import * as OfficeHelpers from '@microsoft/office-js-helpers';
+
+export interface AppProps {
+  title: string;
+  isOfficeInitialized: boolean;
+}
+
+export interface AppState {
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  setColor = async () => {
+    try {
+      await Word.run(async context => {
+        const range = context.document.getSelection();
+        range.load('text');
+        range.font.color = 'green';
+        await context.sync();
+        console.log(`The range address was ${range.text}.`);
+      });
+    } catch (error) {
+      OfficeHelpers.UI.notify(error);
+      OfficeHelpers.Utilities.log(error);
+    }
+  }
+
+  render() {
+    // const {
+    //   title,
+    //   isOfficeInitialized,
+    // } = this.props;
+
+    // if (!isOfficeInitialized) {
+    //   return (
+    //     <Progress
+    //       title={title}
+    //       logo='assets/logo-filled.png'
+    //       message='Please sideload your addin to see app body.'
+    //     />
+    //   );
+    // }
+
+    return (
+      <div className='ms-welcome'>
+        <Header title='Welcome' />
+        <Content message='Choose the button below to set the color of the selected range to green.' buttonLabel='Set color' click={this.setColor} />
+      </div>
+    );
+  }
+}
